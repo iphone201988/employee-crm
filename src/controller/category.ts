202fc7e5f1269525bc4fb3189f4651e69185a7e3
Pardeep Singh <iphone201988@gmail.com>
2025-09-24 12:00:07 +0530

    change

--- src/controller/category.ts
+++ src/controller/category.ts
@@ -125,7 +125,7 @@ const getCategories = async (req: Request, res: Response, next: NextFunction): P
             return;
         } else if (type === "service") {
             const services = await ServicesCategoryModel.find();
-            const clients = await ClientModel.find({"services.0": {$exists: true}}).lean(); 
+            const clients = await ClientModel.find({ "services.0": { $exists: true } }).lean();
             services.forEach((service: any) => {
                 service.count = clients.filter((client: any) => client.services.includes(service._id.toString())).length
             })
@@ -133,9 +133,9 @@ const getCategories = async (req: Request, res: Response, next: NextFunction): P
             return;
         } else if (type === "job") {
             const jobs = await JobCategoryModel.find().lean();
-            const jobList = await JobModel.find().lean();
+            const jobList = await JobModel.find({}, { jobTypeId: 1 }).lean();
             jobs.forEach((job: any) => {
-                job.count = jobList.filter((jobList: any) => jobList.categoryId.toString() === job._id.toString()).length
+                job.count = jobList.filter((jobList: any) => jobList.jobTypeId.toString() === job._id.toString()).length
             })
             SUCCESS(res, 200, "Categories fetched successfully", { data: { jobs } });
             return;
@@ -150,21 +150,42 @@ const getCategories = async (req: Request, res: Response, next: NextFunction): P
             return;
         } else if (type === "bussiness") {
             const bussiness = await BusinessCategoryModel.find().lean();
-            const clients = await ClientModel.find({}, { _id: 1, businessTypeId: 1}).lean();
+            const clients = await ClientModel.find({}, { _id: 1, businessTypeId: 1 }).lean();
             bussiness.forEach((bussiness: any) => {
                 bussiness.count = clients.filter((client: any) => client.businessTypeId.toString() === bussiness._id.toString()).length
             })
             SUCCESS(res, 200, "Categories fetched successfully", { data: { bussiness } });
             return;
-        }  else {
-            const [departments, services, jobs, times, bussiness ] = await Promise.all(
+        } else {
+            const [departments, services, jobs, times, bussiness, userDepartments, clientServices, clientBussiness, jobList, timesheets] = await Promise.all(
                 [
-                    DepartmentCategoryModel.find(),
-                    ServicesCategoryModel.find(),
-                    JobCategoryModel.find(),
-                    TimeCategoryModel.find(),
-                    BusinessCategoryModel.find(),
+                    DepartmentCategoryModel.find().lean(),
+                    ServicesCategoryModel.find().lean(),
+                    JobCategoryModel.find().lean(),
+                    TimeCategoryModel.find().lean(),
+                    BusinessCategoryModel.find().lean(),
+                    UserModel.find({ role: "team" }, { _id: 1, departmentId: 1, }).lean(),
+                    ClientModel.find({ "services.0": { $exists: true } }, { _id: 1, services: 1 }).lean(),
+                    ClientModel.find({}, { _id: 1, businessTypeId: 1 }).lean(),
+                    JobModel.find({}, { jobTypeId: 1 }).lean(),
+                    TimesheetModel.find().lean(),
                 ]);
+            departments.forEach((department: any) => {
+                department.count = userDepartments.filter((team: any) => team.departmentId.toString() === department._id.toString()).length
+            })
+            services.forEach((service: any) => {
+                service.count = clientServices.filter((client: any) => client.services.includes(service._id.toString())).length
+            })
+            jobs.forEach((job: any) => {
+                job.count = jobList.filter((jobList: any) => jobList.jobTypeId.toString() === job._id.toString()).length
+            })
+            times.forEach((time: any) => {
+                time.count = timesheets.filter((timesheet: any) => timesheet.timeId.toString() === time._id.toString()).length
+            })
+            bussiness.forEach((bussiness: any) => {
+                bussiness.count = clientBussiness.filter((client: any) => client.businessTypeId.toString() === bussiness._id.toString()).length
+            })
+            
             SUCCESS(res, 200, "Categories fetched successfully", { data: { departments, services, jobs, times, bussiness } });
             return;
         }
